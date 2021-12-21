91544b5cd7e3b02494e9d851897364afaae28685
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2021-12-21 01:49:28 +0100

    functionality improvement

--- main.js
+++ main.js
@@ -1,98 +1,84 @@
-const allCandidates = "http://localhost:8080/api/candidate/candidates"
+const allCandidates = "http://localhost:8080/api/candidate/candidates";
 fetch(allCandidates)
-    .then(response => response.json())
-    .then(candidates => {
-        for (let i = 0; i < candidates.length; i++) {
-            const candidateDiv = document.getElementById("candidateList")
-    
-                
-            let candidateName = candidates[i].name
-            let candidateParty = candidates[i].party
+  .then((response) => response.json())
+  .then((candidates) => {
+    for (let i = 0; i < candidates.length; i++) {
+      const candidateDiv = document.getElementById("candidateList");
 
-            const partyDiv = document.createElement("p");
-            const nameDiv = document.createElement("h5");
-            
-            const deleteButton = document.createElement("BUTTON");
-    
-            deleteButton.innerHTML = "Delete"
-            
-            deleteButton.addEventListener("click", function() {
-                deleteCandidate(candidates[i].id)
-                alert("Candidate deleted")
-            })
+      let candidateName = candidates[i].name;
+      let candidateParty = candidates[i].party;
 
-            nameDiv.innerHTML = candidateName;
-            partyDiv.innerHTML = candidateParty;
-    
-            
-            candidateDiv.appendChild(nameDiv);
-            candidateDiv.appendChild(partyDiv)
-            candidateDiv.appendChild(deleteButton);
-            
+      const partyDiv = document.createElement("p");
+      const nameDiv = document.createElement("h5");
+
+      const deleteButton = document.createElement("BUTTON");
+      deleteButton.innerHTML = "Delete";
+
+      deleteButton.addEventListener("click", function () {
+        deleteCandidate(candidates[i].id);
+        alert("Candidate deleted");
+      });
+
+      nameDiv.innerHTML = candidateName;
+      partyDiv.innerHTML = candidateParty;
+
+      candidateDiv.appendChild(nameDiv);
+      candidateDiv.appendChild(partyDiv);
+      candidateDiv.appendChild(deleteButton);
     }
-    })
+  });
 
-const addUrl = 'http://localhost:8080/api/candidate';
-
-
-
-const addButton = document.querySelector(".addButton")
-addButton.addEventListener("click", function() {
-    const municipality = document.querySelector(".municipality").value
-    const name = document.querySelector(".name").value
-    const party = document.querySelector(".party").value
-    const zip = document.querySelector(".zip").value
-    
-    addCandidate(municipality, name, party, zip)
-    alert("Candidate added")
-})
-
-// addCandidate("f", "fs", "f2", "f2");
-// deleteCandidate(43);
-
+const addUrl = "http://localhost:8080/api/candidate";
 
 function addCandidate(municipality, name, party, zip) {
-
-const newCandidate = {
+  const newCandidate = {
     municipality: municipality,
     name: name,
     party: party,
     zip: zip,
-}
-const options = {
-    method: 'POST',
-headers: {
-'Content-Type': 'application/json',
-},
-body: JSON.stringify(newCandidate),
-}
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newCandidate),
+  };
 
-fetch(addUrl, options)
-.then(data => {
-    if (!data.ok) {
-      throw Error(data.status);
-     }
-     return data.json();
-    }).then(update => {
-    console.log(update);
-    }).catch(e => {
-    console.log(e);
+  fetch(addUrl, options)
+    .then((data) => {
+      if (!data.ok) {
+        throw Error(data.status);
+      }
+      return data.json();
+    })
+    .then((update) => {
+      console.log(update);
+    })
+    .catch((e) => {
+      console.log(e);
     });
 }
 
+const addButton = document.querySelector(".addButton");
+
+addButton.addEventListener("click", function () {
+  const municipality = document.querySelector(".municipality").value;
+  const name = document.querySelector(".name").value;
+  const party = document.querySelector(".party").value;
+  const zip = document.querySelector(".zip").value;
+
+  addCandidate(municipality, name, party, zip);
+  alert("Candidate added");
+});
+
+
 function deleteCandidate(id) {
-    fetch('http://localhost:8080/api/candidate/' + id, {
-  method: 'DELETE',
-})
-.then(res => res.text()) // or res.json()
-.then(res => console.log(res))
+  fetch("http://localhost:8080/api/candidate/" + id, {
+    method: "DELETE",
+  })
+    .then((res) => res.text()) // or res.json()
+    .then((res) => console.log(res));
 }
 
 
-
-
-    
-
-
-
-    
